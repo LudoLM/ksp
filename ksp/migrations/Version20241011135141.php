0a76396c423a4e34294fe87f4ba2f89f18ee7faf
Debian <debian@vps-fb643710.vps.ovh.net>
2025-02-23 19:04:37 +0000

    deleted Modal

--- ksp/migrations/Version20241011135141.php
+++ ksp/migrations/Version20241011135141.php
@@ -23,11 +23,6 @@ final class Version20241011135141 extends AbstractMigration
         $this->addSql('CREATE TABLE users_cours (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, cours_id INT NOT NULL, is_en_attente TINYINT(1) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_1ABD28D5A76ED395 (user_id), INDEX IDX_1ABD28D57ECF78B0 (cours_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE users_cours ADD CONSTRAINT FK_1ABD28D5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE users_cours ADD CONSTRAINT FK_1ABD28D57ECF78B0 FOREIGN KEY (cours_id) REFERENCES cours (id)');
-        $this->addSql('ALTER TABLE user_cours DROP FOREIGN KEY FK_1F0877C47ECF78B0');
-        $this->addSql('ALTER TABLE user_cours DROP FOREIGN KEY FK_1F0877C4A76ED395');
-        $this->addSql('DROP TABLE user_cours');
-        $this->addSql('DROP INDEX IDX_FDCA8C9C58E536A ON cours');
-        $this->addSql('ALTER TABLE cours DROP users_cours_id, DROP user_cours_id');
     }
 
     public function down(Schema $schema): void
@@ -36,10 +31,5 @@ final class Version20241011135141 extends AbstractMigration
         $this->addSql('CREATE TABLE user_cours (user_id INT NOT NULL, cours_id INT NOT NULL, INDEX IDX_1F0877C4A76ED395 (user_id), INDEX IDX_1F0877C47ECF78B0 (cours_id), PRIMARY KEY(user_id, cours_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE user_cours ADD CONSTRAINT FK_1F0877C47ECF78B0 FOREIGN KEY (cours_id) REFERENCES cours (id) ON UPDATE NO ACTION ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_cours ADD CONSTRAINT FK_1F0877C4A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE users_cours DROP FOREIGN KEY FK_1ABD28D5A76ED395');
-        $this->addSql('ALTER TABLE users_cours DROP FOREIGN KEY FK_1ABD28D57ECF78B0');
-        $this->addSql('DROP TABLE users_cours');
-        $this->addSql('ALTER TABLE cours ADD users_cours_id INT NOT NULL, ADD user_cours_id INT NOT NULL');
-        $this->addSql('CREATE INDEX IDX_FDCA8C9C58E536A ON cours (user_cours_id)');
     }
 }
