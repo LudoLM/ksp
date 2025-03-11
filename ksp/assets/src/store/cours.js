3a3d6011eb8fc071c8c85efaa22f5112c3f3d2e4
Debian <debian@vps-fb643710.vps.ovh.net>
2025-03-11 23:35:20 +0000

    move ksp folder

--- ksp/assets/src/store/cours.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useCoursStore = defineStore("cours", {
-  state: () => ({
-    // Un utilisateur peut s'inscrire à un cours jusqu'à 30 minutes avant le début
-    timeLimiteToSubscribe: 30 * 60 * 1000,
-    timerBeforeBeginning: 6 * 60 * 60 * 1000
-  }),
-});
--- ksp/config/secrets/dev/dev.list.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'DATABASE_URL' => null,
-    'JWT_PASSPHRASE' => null,
-    'JWT_SECRET_KEY' => null,
-    'MAILER_DSN' => null,
-    'STRIPE_PRIVATE_KEY' => null,
-];
