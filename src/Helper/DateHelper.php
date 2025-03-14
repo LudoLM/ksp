55fc07fc7d6b91723a3a5833fac7ff895d84ab50
LudoLM <ludolm@hotmail.fr>
2025-03-14 15:26:06 +0100

    fix FilteringCoursServiceTest.php

---
+++ src/Helper/DateHelper.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helper;
+class DateHelper
+{
+    public static function adjustDatesForCalendarRoute(\DateTime $dateCours, int $currentPage): array
+    {
+        $dateCours->modify('+' . ($currentPage - 1) * 7 . ' days');
+        $dateCours->modify('monday this week');
+        $dateLimit = clone $dateCours;
+        $dateLimit->modify('+6 days');
+
+        return [$dateCours, $dateLimit];
+    }
+}
+
+?>
