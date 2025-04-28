29181491f341ca0791a9de3eb9887ee37d6cdaea
LudoLM <ludolm@hotmail.fr>
2025-04-28 16:19:25 +0200

    refacto .env

--- config/secrets/dev/dev.list.php
+++ config/secrets/dev/dev.list.php
@@ -1,9 +1,15 @@
 <?php
 
 return [
+    'APP_ENV' => null,
+    'APP_SECRET' => null,
+    'APP_URL' => null,
     'DATABASE_URL' => null,
+    'DB_NAME' => null,
+    'DB_PASSWORD' => null,
+    'DB_ROOT_PASSWORD' => null,
+    'DB_USER' => null,
     'JWT_PASSPHRASE' => null,
-    'JWT_SECRET_KEY' => null,
     'MAILER_DSN' => null,
     'STRIPE_PRIVATE_KEY' => null,
 ];
